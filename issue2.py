cd54006ecbf68915345168797679134c22dd572a
GarvitSurana <garvit14surana@gmail.com>
2025-03-01 12:41:14 +0530

    Update issue2.py

--- issue2.py
+++ issue2.py
@@ -1,9 +1,9 @@
 # Make sure program prints all even numbers from 1 to 40
 
 def main():
-    even = 0
-    for i in range(21):
-        even *= 2
+    even = 1
+    for i in range(20):
+        even += 2
         print(even, end="\t")
     print()
 
